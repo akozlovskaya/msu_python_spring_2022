48728ae42255fba360ba131d8245c8e2a22aaf4f
Anastasia Kozlovskaya <95757748+akozlovskaya@users.noreply.github.com>
2022-04-20 00:32:13 +0300

    Add files via upload

---
+++ CustomList/custom_list.py
@@ -0,0 +1,60 @@
+"""  Модуль реализующий пользовательский список. """
+
+
+class CustomList(list):
+    """  Класс реализующий пользовательский список. """
+
+    def __add__(self, other):
+        """ Метод, реализующий левое сложение. """
+        ret = CustomList([a+b for a, b in zip(self, other)])
+        if len(other) > len(self):
+            ret += other[len(self):]
+        if len(other) < len(self):
+            ret += self[len(other):]
+        return ret
+
+    def __radd__(self, other):
+        """ Метод, реализующий правое сложение. """
+        return self + other
+
+    def __sub__(self, other):
+        """ Метод, реализующий левое вычитание. """
+        ret = CustomList([a-b for a, b in zip(self, other)])
+        if len(other) > len(self):
+            ret += [-i for i in other[len(self):]]
+        if len(other) < len(self):
+            ret += self[len(other):]
+        return ret
+
+    def __rsub__(self, other):
+        """ Метод, реализующий правое вычитание. """
+        return CustomList([-i for i in self - other])
+
+    def __eq__(self, other):
+        """ Метод, реализующий оператор равенства. """
+        return sum(self) == sum(other)
+
+    def __ne__(self, other):
+        """ Метод, реализующий оператор неравенства. """
+        return sum(self) != sum(other)
+
+    def __lt__(self, other):
+        """ Метод, реализующий оператор «меньше». """
+        return sum(self) < sum(other)
+
+    def __le__(self, other):
+        """ Метод, реализующий оператор «меньше или равно». """
+        return sum(self) <= sum(other)
+
+    def __gt__(self, other):
+        """ Метод, реализующий оператор «больше». """
+        return sum(self) > sum(other)
+
+    def __ge__(self, other):
+        """ Метод, реализующий оператор «больше или равно». """
+        return sum(self) >= sum(other)
+
+    def __str__(self):
+        """ Метод, реализующий строковое представление CustomList. """
+        items = ', '.join([str(i) for i in self])
+        return f'items = [{items}], sum = {sum(self)}'
--- CustomList/test.py
+++ CustomList/test.py
@@ -1,59 +1,66 @@
+"""  Модуль реализующий тесты для CustomList. """
+
 import unittest
 
-from CustomList import CustomList
+from custom_list import CustomList
 
 
 class CustomListTestClass(unittest.TestCase):
+    """  Класс реализующий тесты для CustomList. """
 
     def test_str(self):
+        """  Метод, тестирующий строковое представление CustomList. """
         custom_list = CustomList([5, 1, 3, 7])
         self.assertEqual(str(custom_list), "items = [5, 1, 3, 7], sum = 16")
-        
+
         custom_list = CustomList([5])
         self.assertEqual(str(custom_list), "items = [5], sum = 5")
-        
+
         custom_list = CustomList([])
         self.assertEqual(str(custom_list), "items = [], sum = 0")
-    
+
     # так как сравнение списков перегружено, далее будем использовать
     # сравнение str(CustomList), убедившись в корректной работе этого метода
     def test_add(self):
+        """  Метод, тестирующий сложение CustomList и CustomList/list. """
         add_result = CustomList([5, 1, 3, 7]) + CustomList([1, 2, 7])
         true_result = CustomList([6, 3, 10, 7])
         self.assertEqual(str(add_result), str(true_result))
-        
+
         add_result = [1, 2] + CustomList([3, 4])
         true_result = CustomList([4, 6])
         self.assertEqual(str(add_result), str(true_result))
-        
+
         add_result = CustomList([3, 4]) + [1, 2]
         true_result = CustomList([4, 6])
         self.assertEqual(str(add_result), str(true_result))
-        
+
     def test_sub(self):
+        """  Метод, тестирующий вычитание CustomList и CustomList/list. """
         add_result = CustomList([5, 1, 3, 7]) - CustomList([1, 2, 7])
         true_result = CustomList([4, -1, -4, 7])
         self.assertEqual(str(add_result), str(true_result))
-        
+
         add_result = [1, 2] - CustomList([3, 4])
         true_result = CustomList([-2, -2])
         self.assertEqual(str(add_result), str(true_result))
-        
+
         add_result = CustomList([3, 4]) - [1, 2]
         true_result = CustomList([2, 2])
         self.assertEqual(str(add_result), str(true_result))
-      
+
     def test_comparison(self):
+        """  Метод, тестирующий сравнение CustomList. """
         list_sum_16 = CustomList([5, 1, 3, 7])
         list_sum_16_ = CustomList([1, 8, 7])
         list_sum_8 = CustomList([1, 7])
-        
+
         self.assertTrue(list_sum_16 == list_sum_16_)
         self.assertFalse(list_sum_16 == list_sum_8)
-        
+
         self.assertTrue(list_sum_16 != list_sum_8)
         self.assertFalse(list_sum_16 != list_sum_16_)
-        
+
         self.assertFalse(list_sum_8 > list_sum_16)
         self.assertTrue(list_sum_16 >= list_sum_16_)
         self.assertFalse(list_sum_16 <= list_sum_8)
