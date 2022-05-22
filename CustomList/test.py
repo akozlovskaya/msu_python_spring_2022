bae0d914c766bdf6c7ab624d80dc052f53978be9
Anastasia Kozlovskaya <95757748+akozlovskaya@users.noreply.github.com>
2022-05-22 14:28:41 +0300

    Add files via upload

--- CustomList/test.py
+++ CustomList/test.py
@@ -1,12 +1,17 @@
-"""  Модуль, реализующий тесты для CustomList. """
+"""  Модуль реализующий тесты для CustomList. """
 
 import unittest
 
 from custom_list import CustomList
 
+def test_equal(test_class, list1, list2):
+    test_class.assertEqual(len(list1), len(list2))
+    for i in range(len(list1)):
+        test_class.assertEqual(list1[i], list2[i])
+
 
 class CustomListTestClass(unittest.TestCase):
-    """  Класс, реализующий тесты для CustomList. """
+    """  Класс реализующий тесты для CustomList. """
 
     def test_str(self):
         """  Метод, тестирующий строковое представление CustomList. """
@@ -19,35 +24,201 @@ class CustomListTestClass(unittest.TestCase):
         custom_list = CustomList([])
         self.assertEqual(str(custom_list), "items = [], sum = 0")
 
-    # так как сравнение списков перегружено, далее будем использовать
-    # сравнение str(CustomList), убедившись в корректной работе этого метода
-    def test_add(self):
-        """  Метод, тестирующий сложение CustomList и CustomList/list. """
+    def test_add_сustl_custl(self):
+        """  Метод, тестирующий сложение CustomList и CustomList. """
+
+        # a + b: len(a) == len(b)
+        list_1 = CustomList([5, 1, 3, 7])
+        list_2 = CustomList([1, 2, 7, 5])
+        add_result = list_1 + list_2
+        true_result = CustomList([6, 3, 10, 12])
+        test_equal(self, add_result, true_result)
+        test_equal(self, list_1, [5, 1, 3, 7])
+        test_equal(self, list_2, [1, 2, 7, 5])
+        self.assertEqual(type(add_result), CustomList)
+        
+
+        # a + b: len(a) < len(b)
+        list_1 = CustomList([5, 1, 3, 7])
+        list_2 = CustomList([1, 2, 7])
         add_result = CustomList([5, 1, 3, 7]) + CustomList([1, 2, 7])
         true_result = CustomList([6, 3, 10, 7])
-        self.assertEqual(str(add_result), str(true_result))
+        test_equal(self, add_result, true_result)
+        test_equal(self, list_1, [5, 1, 3, 7])
+        test_equal(self, list_2, [1, 2, 7])
+        self.assertEqual(type(add_result), CustomList)
 
-        add_result = [1, 2] + CustomList([3, 4])
+        # a + b: len(a) > len(b)
+        add_result = list_2 + list_1
+        true_result = CustomList([6, 3, 10, 7])
+        test_equal(self, add_result, true_result)
+        test_equal(self, list_1, [5, 1, 3, 7])
+        test_equal(self, list_2, [1, 2, 7])
+        self.assertEqual(type(add_result), CustomList)
+    
+    def test_сustl_list(self):
+        """  Метод, тестирующий сложение CustomList и List. """
+
+        # a + b: len(a) == len(b)
+        list_1 = CustomList([3, 4])
+        list_2 = [1, 2]
+        add_result = list_1 + list_2
         true_result = CustomList([4, 6])
-        self.assertEqual(str(add_result), str(true_result))
+        test_equal(self, add_result, true_result)
+        test_equal(self, list_1, [3, 4])
+        test_equal(self, list_2, [1, 2])
+        self.assertEqual(type(add_result), CustomList)
+        
+        # a + b: len(a) < len(b)
+        list_1 = CustomList([3, 4])
+        list_2 = [1, 2, 5]
+        add_result = list_1 + list_2
+        true_result = CustomList([4, 6, 5])
+        test_equal(self, add_result, true_result)
+        test_equal(self, list_1, [3, 4])
+        test_equal(self, list_2, [1, 2, 5])
+        self.assertEqual(type(add_result), CustomList)
+        
+        # a + b: len(a) > len(b)
+        list_1 = CustomList([3, 4])
+        list_2 = [1]
+        add_result = list_1 + list_2
+        true_result = CustomList([4, 4])
+        test_equal(self, add_result, true_result)
+        test_equal(self, list_1, [3, 4])
+        test_equal(self, list_2, [1])
+        self.assertEqual(type(add_result), CustomList)
 
-        add_result = CustomList([3, 4]) + [1, 2]
-        true_result = CustomList([4, 6])
-        self.assertEqual(str(add_result), str(true_result))
+    def test_add_list_custl(self):
+        """  Метод, тестирующий сложение List и CustomList. """
 
-    def test_sub(self):
-        """  Метод, тестирующий вычитание CustomList и CustomList/list. """
-        add_result = CustomList([5, 1, 3, 7]) - CustomList([1, 2, 7])
+        # a + b: len(a) == len(b)
+        list_1 = [5, 1, 3, 7]
+        list_2 = CustomList([1, 2, 7, 5])
+        add_result = list_1 + list_2
+        true_result = CustomList([6, 3, 10, 12])
+        test_equal(self, add_result, true_result)
+        test_equal(self, list_1, [5, 1, 3, 7])
+        test_equal(self, list_2, [1, 2, 7, 5])
+        self.assertEqual(type(add_result), CustomList)
+
+        # a + b: len(a) < len(b)
+        list_2 = CustomList([1, 2, 7])
+        add_result = list_1 + list_2
+        true_result = CustomList([6, 3, 10, 7])
+        test_equal(self, add_result, true_result)
+        test_equal(self, list_1, [5, 1, 3, 7])
+        test_equal(self, list_2, [1, 2, 7])
+        self.assertEqual(type(add_result), CustomList)
+
+        # a + b: len(a) > len(b)
+        list_1 = [1, 2]
+        list_2 = CustomList([3, 4, 5])
+        add_result = list_1 + list_2
+        true_result = CustomList([4, 6, 5])
+        test_equal(self, add_result, true_result)
+        test_equal(self, list_1, [1, 2])
+        test_equal(self, list_2, [3, 4, 5])
+        self.assertEqual(type(add_result), CustomList)
+
+    def test_sub_сustl_custl(self):
+        """  Метод, тестирующий вычитание из CustomList CustomList. """
+
+        # a - b: len(a) == len(b)
+        list_1 = CustomList([5, 1, 3, 7])
+        list_2 = CustomList([1, 2, 7, 5])
+        sub_result = list_1 - list_2
+        true_result = CustomList([4, -1, -4, 2])
+        test_equal(self, sub_result, true_result)
+        test_equal(self, list_1, CustomList([5, 1, 3, 7]))
+        test_equal(self, list_2, CustomList([1, 2, 7, 5]))
+        self.assertEqual(type(sub_result), CustomList)
+
+        # a - b: len(a) > len(b)
+        list_1 = CustomList([5, 1, 3, 7])
+        list_2 = CustomList([1, 2, 7])
+        sub_result = list_1 - list_2
         true_result = CustomList([4, -1, -4, 7])
-        self.assertEqual(str(add_result), str(true_result))
+        test_equal(self, sub_result, true_result)
+        test_equal(self, list_1, CustomList([5, 1, 3, 7]))
+        test_equal(self, list_2, CustomList([1, 2, 7]))
+        self.assertEqual(type(sub_result), CustomList)
 
-        add_result = [1, 2] - CustomList([3, 4])
-        true_result = CustomList([-2, -2])
-        self.assertEqual(str(add_result), str(true_result))
+        # a - b: len(a) < len(b)
+        list_1 = CustomList([5, 1, 3])
+        list_2 = CustomList([1, 2, 7, 5])
+        sub_result = list_1 - list_2
+        true_result = CustomList([4, -1, -4, -5])
+        test_equal(self, sub_result, true_result)
+        test_equal(self, list_1, CustomList([5, 1, 3]))
+        test_equal(self, list_2, CustomList([1, 2, 7, 5]))
+        self.assertEqual(type(sub_result), CustomList)
 
-        add_result = CustomList([3, 4]) - [1, 2]
-        true_result = CustomList([2, 2])
-        self.assertEqual(str(add_result), str(true_result))
+    def test_sub_сustl_list(self):
+        """  Метод, тестирующий вычитание из CustomList List. """
+
+        # a - b: len(a) == len(b)
+        list_1 = CustomList([5, 1, 3, 7])
+        list_2 = [1, 2, 7, 5]
+        sub_result = list_1 - list_2
+        true_result = CustomList([4, -1, -4, 2])
+        test_equal(self, sub_result, true_result)
+        test_equal(self, list_1, CustomList([5, 1, 3, 7]))
+        test_equal(self, list_2, [1, 2, 7, 5])
+        self.assertEqual(type(sub_result), CustomList)
+
+        # a - b: len(a) > len(b)
+        list_1 = CustomList([5, 1, 3, 7])
+        list_2 = [1, 2, 7]
+        sub_result = list_1 - list_2
+        true_result = CustomList([4, -1, -4, 7])
+        test_equal(self, sub_result, true_result)
+        test_equal(self, list_1, CustomList([5, 1, 3, 7]))
+        test_equal(self, list_2, [1, 2, 7])
+        self.assertEqual(type(sub_result), CustomList)
+
+        # a - b: len(a) < len(b)
+        list_1 = CustomList([5, 1, 3])
+        list_2 = [1, 2, 7, 5]
+        sub_result = list_1 - list_2
+        true_result = CustomList([4, -1, -4, -5])
+        test_equal(self, sub_result, true_result)
+        test_equal(self, list_1, CustomList([5, 1, 3]))
+        test_equal(self, list_2, [1, 2, 7, 5])   
+        self.assertEqual(type(sub_result), CustomList) 
+
+    def test_sub_list_custl(self):
+        """  Метод, тестирующий вычитание из List CustomList. """
+
+        # a - b: len(a) == len(b)
+        list_1 = [5, 1, 3, 7]
+        list_2 = CustomList([1, 2, 7, 5])
+        sub_result = list_1 - list_2
+        true_result = CustomList([4, -1, -4, 2])
+        test_equal(self, sub_result, true_result)
+        test_equal(self, list_1, [5, 1, 3, 7])
+        test_equal(self, list_2, CustomList([1, 2, 7, 5]))
+        self.assertEqual(type(sub_result), CustomList)
+
+        # a - b: len(a) > len(b)
+        list_1 = [5, 1, 3, 7]
+        list_2 = CustomList([1, 2, 7])
+        sub_result = list_1 - list_2
+        true_result = CustomList([4, -1, -4, 7])
+        test_equal(self, sub_result, true_result)
+        test_equal(self, list_1, [5, 1, 3, 7])
+        test_equal(self, list_2, CustomList([1, 2, 7]))
+        self.assertEqual(type(sub_result), CustomList)
+
+        # a - b: len(a) < len(b)
+        list_1 = [5, 1, 3]
+        list_2 = CustomList([1, 2, 7, 5])
+        sub_result = list_1 - list_2
+        true_result = CustomList([4, -1, -4, -5])
+        test_equal(self, sub_result, true_result)
+        test_equal(self, list_1, [5, 1, 3])
+        test_equal(self, list_2, CustomList([1, 2, 7, 5]))
+        self.assertEqual(type(sub_result), CustomList)
 
     def test_comparison(self):
         """  Метод, тестирующий сравнение CustomList. """
