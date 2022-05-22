2c190c37b978b2780d005cbef480e4eaae2a63fe
Anastasia Kozlovskaya <95757748+akozlovskaya@users.noreply.github.com>
2022-05-22 13:36:11 +0300

    Add files via upload

--- Fetcher/test.py
+++ Fetcher/test.py
@@ -13,17 +13,12 @@ from fetcher import crawl
 class MyAppTestCase(AioHTTPTestCase):
     """ Test class """
 
-    def setUp(self):
+    async def test_1(self):
+        """ Time test """
         self.devnull = open(os.devnull, "w", encoding="utf8")
         self.old_stdout = sys.stdout
         sys.stdout = self.devnull
 
-    def tearDown(self):
-        sys.stdout = self.old_stdout
-        self.devnull.close()
-
-    async def test_1(self):
-        """ Time test """
         counter_1 = time.perf_counter()
         await crawl(threads=5, filename='urls1.txt', qsize=5)
         counter_2 = time.perf_counter()
@@ -39,14 +34,15 @@ class MyAppTestCase(AioHTTPTestCase):
         counter_2 = time.perf_counter()
         time_3 = counter_2 - counter_1
 
+        sys.stdout = self.old_stdout
+        self.devnull.close()
+
         self.assertTrue(time_1 > time_2)
         self.assertTrue(time_2 > time_3)
 
 
 if __name__ == '__main__':
     my_test = MyAppTestCase()
-    my_test.setUp()
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(my_test.test_1())
-    my_test.tearDown()
